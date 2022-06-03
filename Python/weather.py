ba7567ae716699b33833964c52bfd55a2f4c89a3
Lesnar Dsilva <lesnar.dsilva@gmail.com>
2022-06-03 11:51:19 +0100

    Update weather.py

--- Python/weather.py
+++ Python/weather.py
@@ -1,7 +1,7 @@
 import requests;#web requests
 from pprint import pprint;#parse JSON
 
-key = "f3b4ddef37f73517773672b92135a94d";
+key = "";#API Key
 city = input("Enter a city: ");
 api = f"https://api.openweathermap.org/data/2.5/weather?appid={key}&q={city}";
 weather_data = requests.get(api).json();
