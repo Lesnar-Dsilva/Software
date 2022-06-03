f54a3db83ae0aa6c62f3df87d9af6ae7670d5c9b
Lesnar Dsilva <lesnar.dsilva@gmail.com>
2022-06-03 11:51:37 +0100

    Update url_shortner(cuttly).py

--- Python/url_shortner(cuttly).py
+++ Python/url_shortner(cuttly).py
@@ -1,5 +1,5 @@
 import requests
-key = "9c87439bb5d616d84ddc05d416e7866ffb3e7";
+key = "";#API Key
 #"https://" REQUIRED
 url = "https://www.google.com";
 api = f"https://cutt.ly/api/api.php?key={key}&short={url}";
